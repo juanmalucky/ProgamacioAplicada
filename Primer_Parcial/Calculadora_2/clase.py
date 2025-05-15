a0d5658ae7dd0fb8a5762a27bb7433b9ea5cfc5b
juanmalucky <150405173+juanmalucky@users.noreply.github.com>
2025-05-14 19:22:37 -0500

    clase.py

---
+++ Primer_Parcial/Calculadora_2/clase.py
@@ -0,0 +1,23 @@
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def sumar(self):
+        return self.num1 + self.num2
+
+    def restar(self):
+        return self.num1 - self.num2
+
+    def multiplicar(self):
+        return self.num1 * self.num2
+
+    def dividir(self):
+        try:
+            resultado = self.num1 / self.num2
+            return resultado
+        except ZeroDivisionError:
+            return "Error: No se puede dividir por cero"
+
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
