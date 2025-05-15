923da0bdbbc5daed4bedc6ad6177157cc47e91e6
juanmalucky <150405173+juanmalucky@users.noreply.github.com>
2025-05-14 20:15:10 -0500

    main.py

--- Primer_Parcial/Calculadora_2/main.py
+++ Primer_Parcial/Calculadora_2/main.py
@@ -1,7 +1,13 @@
-from clase import Calculadora
+from calculadora import Calculadora  # Importamos la clase desde el archivo calculadora.py
 
+# Pedimos al usuario que ingrese dos números
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
+
+# Creamos un objeto de la clase Calculadora con los números dados
 calc = Calculadora(a, b)
 
+# Mostramos los resultados de las operaciones
 print("El resultado de la suma es:", calc.sumar())
 print("El resultado de la resta es:", calc.restar())
 print("El resultado de la multiplicacion es:", calc.multiplicar())
