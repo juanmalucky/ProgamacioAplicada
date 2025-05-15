6486e9c7e92c23048d7bc89ef569914dd6effaf5
juanmalucky <150405173+juanmalucky@users.noreply.github.com>
2025-05-14 19:23:48 -0500

    main.py

--- Primer_Parcial/Calculadora_2/main.py
+++ Primer_Parcial/Calculadora_2/main.py
@@ -1,1 +1,6 @@
+calc = Calculadora(a, b)
 
+print("El resultado de la suma es:", calc.sumar())
+print("El resultado de la resta es:", calc.restar())
+print("El resultado de la multiplicacion es:", calc.multiplicar())
+print("El resultado de la division es:", calc.dividir())
